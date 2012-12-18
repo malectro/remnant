5799713a7f6718553e9a63318c7ac9b2575fefb4
Kyle Warren <qarren@gmail.com>
2012-12-18 11:03:32 -0500

    fixed stuff

--- hud.js
+++ hud.js
@@ -10,12 +10,13 @@
   me.init = function () {
     _div = doc.createElement('div');
 
-    _.extend(_div, {
+    _.extend(_div.style, {
       position: 'absolute',
-      top: 10,
-      right: 10,
+      top: '5px',
+      right: '10px',
       textAlign: 'right',
-      color: 'white'
+      color: 'white',
+      fontFamily: 'courier'
     });
 
     doc.body.appendChild(_div);
